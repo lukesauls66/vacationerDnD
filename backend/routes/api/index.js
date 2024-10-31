c2b2196afbed00c56ad66ee94f0fca4dbf12cde1
Luke Sauls <luke.sauls279@gmail.com>
2024-10-30 17:13:29 -0700

    just have to refactor

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -15,14 +15,4 @@ router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
 });
 
-// router.get("/set-token-cookie", async (_req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: "Demo-lition",
-//     },
-//   });
-//   setTokenCookie(res, user);
-//   return res.json({ user: user });
-// });
-
 module.exports = router;
