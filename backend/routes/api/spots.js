e241266719f0945322e3e6e5867ff68de2d0efd4
glitterpillz <karenhickey1010@gmail.com>
2024-11-02 16:35:12 -0400

    completed route handler: get '/spots' -> returns all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,2 +1,20 @@
 const router = require("express").Router();
+
+const { Spot } = require('../../db/models')
+
+
+// ### Get all Spots
+
+router.get('/', async (req, res) => {
+    try {
+        const spots = await Spot.findAll();
+        res.status(200).json({ Spots: spots})
+    } catch (err) {
+        res.status(500).json({
+            message: 'Unexpected error: skill issue'
+        });
+    };
+});
+
+
 module.exports = router;
