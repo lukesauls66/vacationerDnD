d61e669c1490fb9d0fb681ef2f2b45b9653cc2bd
Luke Sauls <luke.sauls279@gmail.com>
2024-11-05 12:38:15 -0700

    changing user syntax for render

--- backend/db/migrations/20241030131301-create-user.js
+++ backend/db/migrations/20241030131301-create-user.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable(
-      "Users",
+      "users",
       {
         id: {
           allowNull: false,
@@ -52,7 +52,7 @@ module.exports = {
     );
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = "Users";
+    options.tableName = "users";
     await queryInterface.dropTable(options);
   },
 };
