1f7530a391632d432a5c1f046cb4b127546eb70f
Luke Sauls <luke.sauls279@gmail.com>
2024-11-05 15:26:55 -0700

    got rid of add column migration

--- backend/db/migrations/20241101194821-create-spot.js
+++ backend/db/migrations/20241101194821-create-spot.js
@@ -15,6 +15,15 @@ module.exports = {
           primaryKey: true,
           type: Sequelize.INTEGER,
         },
+        ownerId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          references: {
+            model: "Users",
+            key: "id",
+          },
+          onDelete: "CASCADE",
+        },
         address: {
           type: Sequelize.STRING,
           allowNull: false,
