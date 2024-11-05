3aa1da0ebd637fe47fa1f09adcb0b2947ce63acd
Luke Sauls <luke.sauls279@gmail.com>
2024-11-05 12:35:04 -0700

    changing user syntax for render

--- backend/db/migrations/20241102234358-add-ownerId-to-spots.js
+++ backend/db/migrations/20241102234358-add-ownerId-to-spots.js
@@ -7,7 +7,7 @@ module.exports = {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {
-        model: "User",
+        model: "users",
         key: "id",
       },
     });
