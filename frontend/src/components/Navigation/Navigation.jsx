c3ce9fe9ca8c3c8a82f41735e34a2625c2aa70a1
Luke Sauls <luke.sauls279@gmail.com>
2024-11-20 12:08:48 -0700

    done with phase 4, working on drop downs for logged in and logged out

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,1 +1,49 @@
-function Navigation() {}
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import OpenModalButton from "../OpenModalButton/OpenModalButton";
+import LoginFormModal from "../LoginFormModal/LoginFormModal";
+import SignupFormModal from "../SignupFormModal/SignupFormModal";
+import { FcHome } from "react-icons/fc";
+import "./Navigation.css";
+
+function Navigation({ isUserLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
+
+  const sessionLinks = sessionUser ? (
+    <li>
+      <ProfileButton user={sessionUser} />
+    </li>
+  ) : (
+    <div className="login-signup-container">
+      <li>
+        <OpenModalButton
+          buttonText="Log In"
+          modalComponent={<LoginFormModal />}
+        />
+      </li>
+      <li>
+        <OpenModalButton
+          buttonText="Sign Up"
+          modalComponent={<SignupFormModal />}
+        />
+      </li>
+    </div>
+  );
+
+  return (
+    <ul className="nav-container">
+      <li id="home-link-container">
+        <div id="home-icon-container">
+          <FcHome />
+        </div>
+        <NavLink to="/" id="home-link" className={"link"}>
+          VacayDnD
+        </NavLink>
+      </li>
+      {isUserLoaded && sessionLinks}
+    </ul>
+  );
+}
+
+export default Navigation;
