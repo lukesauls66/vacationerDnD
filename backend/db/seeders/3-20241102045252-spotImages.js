c380744af2aec7f77fa8afa35e0f6ab830188f9d
Luke Sauls <luke.sauls279@gmail.com>
2024-11-21 10:36:32 -0700

    updated users and spots

--- backend/db/seeders/3-20241102045252-spotImages.js
+++ backend/db/seeders/3-20241102045252-spotImages.js
@@ -12,27 +12,42 @@ module.exports = {
       [
         {
           spotId: 1,
-          url: "https://example.com/images/spot1.jpg",
+          url: "https://drive.google.com/uc?export=view&id=1qhPaAZFcmMGK4Dt3dftdBXynNphAwlmn",
           preview: true,
         },
         {
           spotId: 2,
-          url: "https://example.com/images/spot2.jpg",
-          preview: false,
+          url: "https://drive.google.com/uc?export=view&id=1PxI2xHObFqjNee6gbAu18LRPREBvv7dJ",
+          preview: true,
         },
         {
           spotId: 3,
-          url: "https://example.com/images/spot3.jpg",
+          url: "https://drive.google.com/uc?export=view&id=1yScyURSUdUoMzeme2DaULIU9bzP1vclw",
           preview: true,
         },
         {
           spotId: 4,
-          url: "https://example.com/images/spot4.jpg",
-          preview: false,
+          url: "https://drive.google.com/uc?export=view&id=125ExNygoI-LZVa8KFps72H5lQBIAcDPH",
+          preview: true,
         },
         {
           spotId: 5,
-          url: "https://example.com/images/spot5.jpg",
+          url: "https://drive.google.com/uc?export=view&id=1sx82ydaBlyAyjf8WTEpU-ULhmci3gp-J",
+          preview: true,
+        },
+        {
+          spotId: 6,
+          url: "https://drive.google.com/uc?export=view&id=1gtCjo3DFKAPGHbHQO-3u9TsrWDMQlTSl",
+          preview: true,
+        },
+        {
+          spotId: 7,
+          url: "https://drive.google.com/uc?export=view&id=1SKtjKcs2HrsprgwKCcwpCfwtzYpM_6po",
+          preview: true,
+        },
+        {
+          spotId: 8,
+          url: "https://drive.google.com/uc?export=view&id=1bCXvQFvfw5NbeaTqbtp7yfzAGV9awoJO",
           preview: true,
         },
       ],
